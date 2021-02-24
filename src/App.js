25ea5f728758e8eddd73503e4a743c23e336caed
quentin-papera <qpapera@gmail.com>
2021-02-25 01:48:10 +1300

    added navbar with styling/added assets folder with images and icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+
+//Components 
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      My Portfolio
+      <MyNavbar/>
     </div>
   );
 }
